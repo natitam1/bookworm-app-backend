867b971800eb0b1ac7f3ad597a9bed7637295d29
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-11-21 04:37:15 +0300

    chore: add updated at

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -54,6 +54,7 @@ router.post("/register", async (req, res) => {
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: user.createdAt,
       },
     });
   } catch (error) {
@@ -86,6 +87,7 @@ router.post("/login", async (req, res) => {
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: new Date(),
       },
     });
   } catch (error) {
